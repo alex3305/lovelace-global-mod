8025be516a804b5e3cf54e9cb9a557fe42b047cd
Alex van den Hoogen <alex3305@gmail.com>
2024-12-01 16:44:19 +0100

    Cleanup code, move to this

--- src/global-mod.js
+++ src/global-mod.js
@@ -4,64 +4,64 @@ class GlobalMod {
 
     static instance;
     
-    hass;
+    #hass;
 
-    config;
+    #config;
 
-    styles;
+    #styles;
     
     constructor() {
-        GlobalMod.instance = this;
-        GlobalMod.instance.styles = [];
-
-        GlobalMod.instance.refreshHomeAssistant(),
+        console.time('Component Load');
+        this.#styles = [];
+        this.refreshHomeAssistant(),
 
         Promise.all([
-            GlobalMod.instance.loadConfig(),
-            GlobalMod.instance.addEventListeners()
+            this.loadConfig(),
+            this.addEventListeners()
         ]);
+        console.timeEnd('Component Load');
     }
 
     static get Current() { return window.location.pathname.toLowerCase(); }
 
-    static get DarkMode() { return GlobalMod.instance.hass.themes.darkMode; }
-    
     static get EditMode() { return GlobalMod.Current.includes('?edit=1'); }
     
     static get Name() { return name; }
-
+    
     static get Version() { return version; }
+    
+    get darkMode() { return this.#hass.themes.darkMode; }
 
     async addEventListeners() {
         // Listen to location-changed for navigation.
         // Wrapped in setTimeout to try and execute last.
         // Reference: https://github.com/home-assistant/frontend/blob/fa03c58a93219ad008df3806ac50d2fd893ad87d/hassio/src/hassio-main.ts#L49-L56
-        window.addEventListener('location-changed', () => setTimeout(() => GlobalMod.instance.applyStyles()), false);
+        window.addEventListener('location-changed', () => setTimeout(() => this.applyStyles()), false);
 
         // Listen to popstate for history tracking.
-        window.addEventListener('popstate', () => GlobalMod.instance.applyStyles(), false);
+        window.addEventListener('popstate', () => this.applyStyles(), false);
 
         // Listen to visibility change (ie. re-focus) for scroll changes
         // Reference: https://github.com/home-assistant/frontend/issues/20854
         document.addEventListener('visibilitychange', () => {
             if (!document.hidden) {
-                GlobalMod.instance.applyStyles();
+                this.applyStyles();
             }
         }, false);
 
         // Listen to click event on document body for navigation.
         // Reference: https://github.com/home-assistant/frontend/blob/fa03c58a93219ad008df3806ac50d2fd893ad87d/hassio/src/hassio-main.ts#L58-L65
-        document.body.addEventListener('click', () => GlobalMod.instance.applyStyles(), false);
+        document.body.addEventListener('click', () => this.applyStyles(), false);
 
         // Listen to settheme events for when themes change and also reload hass object.
         // Reference: https://github.com/thomasloven/lovelace-card-mod/blob/f59abc785eabb689ac42a9076197254421f96c60/src/theme-watcher.ts
         document.querySelector('hc-main')?.addEventListener('settheme', () => {
-            GlobalMod.instance.refreshHomeAssistant();
-            GlobalMod.instance.applyStyles();
+            this.refreshHomeAssistant();
+            this.applyStyles();
         }, false);
         document.querySelector('home-assistant')?.addEventListener('settheme', () => {
-            GlobalMod.instance.refreshHomeAssistant();
-            GlobalMod.instance.applyStyles();
+            this.refreshHomeAssistant();
+            this.applyStyles();
         }, false);
     }
 
@@ -74,7 +74,7 @@ class GlobalMod {
 
         style.textContent += rule.style;
         if (rule.darkStyle !== undefined || rule.lightStyle !== undefined) {
-            style.textContent += GlobalMod.DarkMode ? 
+            style.textContent += this.darkMode ? 
                 rule.darkStyle : rule.lightStyle;
         }
 
@@ -95,16 +95,16 @@ class GlobalMod {
         }
 
         if (style === undefined) {
-            style = await GlobalMod.instance.createStyleElement(rule);
+            style = await this.createStyleElement(rule);
             style?.classList?.add(name);
         }
 
         try {
-            const tree = await GlobalMod.instance.selectTree(`home-assistant$${rule.selector}`, 1, 9);
+            const tree = await this.selectTree(`home-assistant$${rule.selector}`, 1, 9);
             
             if (!tree.contains(style)) {
                 tree.appendChild(style);
-                GlobalMod.instance.styles.push(style);
+                this.#styles.push(style);
             }
         } catch {
             console.error(`Could not create rule ${name} after multiple tries...`);
@@ -115,26 +115,26 @@ class GlobalMod {
         const current = GlobalMod.Current;
         const editMode = GlobalMod.EditMode;
 
-        for await (const [name, rule] of GlobalMod.instance.config) {
-            let style = GlobalMod.instance.styles.find(e => e.classList?.contains(name));
-            GlobalMod.instance.applyStyle(name, rule, current, editMode, style);
+        for await (const [name, rule] of this.#config) {
+            let style = this.#styles.find(e => e.classList?.contains(name));
+            this.applyStyle(name, rule, current, editMode, style);
         }
     }
 
     async loadConfig() {
-        let currentTheme = `${GlobalMod.instance.hass.themes.theme}-${GlobalMod.Name}`;
+        let currentTheme = `${this.#hass.themes.theme}-${GlobalMod.Name}`;
 
         // Fall back to theme name without global-mod suffix. This should be temporary.
-        if (!(currentTheme in GlobalMod.instance.hass.themes.themes)) {
-            currentTheme = `${GlobalMod.instance.hass.themes.theme}`;
+        if (!(currentTheme in this.#hass.themes.themes)) {
+            currentTheme = `${this.#hass.themes.theme}`;
         } else {
             console.warn(`Theme still uses the deprecated ${GlobalMod.Name}-suffix.`);
         }
 
-        const theme = GlobalMod.instance.hass.themes.themes[currentTheme];
-        GlobalMod.instance.config = await GlobalMod.instance.loadRules(theme);
+        const theme = this.#hass.themes.themes[currentTheme];
+        this.#config = await this.loadRules(theme);
 
-        if (!GlobalMod.instance.config || GlobalMod.instance.config.size == 0) {
+        if (!this.#config || this.#config.size == 0) {
             console.info(`%c Global mod %c loaded without any config... \n  👉 Add a 'mods' section to your theme %c ${currentTheme} %c to enable modding.`,
                     'color:white;background:purple;', '', 'color:black;background:lightblue;', '');
         } else {
@@ -159,14 +159,14 @@ class GlobalMod {
             };
 
             rules.set(ruleKey, rule);
-            GlobalMod.instance.applyStyle(ruleKey, rule, current, editMode);
+            this.applyStyle(ruleKey, rule, current, editMode);
         }
 
         return rules;
     }
 
     refreshHomeAssistant() {
-        GlobalMod.instance.hass = document.querySelector('home-assistant').hass;
+        this.#hass = document.querySelector('home-assistant').hass;
     }
 
     async selectTree(selector, iterations, maxIterations) {
@@ -200,7 +200,7 @@ class GlobalMod {
             }
 
             await new Promise((resolve) => setTimeout(resolve, iterations * 10));
-            return await GlobalMod.instance.selectTree(selector, iterations + 1, maxIterations);
+            return await this.selectTree(selector, iterations + 1, maxIterations);
         }
     }
 }
