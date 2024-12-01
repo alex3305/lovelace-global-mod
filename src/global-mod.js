4d9d4f5bb9edc5c5f8bf57371b0babded0f67f37
Alex van den Hoogen <alex3305@gmail.com>
2024-12-01 10:36:40 +0100

    Improved performance

--- src/global-mod.js
+++ src/global-mod.js
@@ -13,15 +13,17 @@ class GlobalMod {
     constructor() {
         GlobalMod.instance = this;
         GlobalMod.instance.styles = [];
-        
         GlobalMod.instance.refreshHomeAssistant();
+    
         GlobalMod.instance.loadConfig();
-        GlobalMod.instance.applyStyles();
-
         GlobalMod.instance.addEventListeners();
     }
 
+    static get Current() { return window.location.pathname.toLowerCase(); }
+
     static get DarkMode() { return GlobalMod.instance.hass.themes.darkMode; }
+    
+    static get EditMode() { return window.location.search.includes('?edit=1'); }
     
     static get Name() { return name; }
 
@@ -67,71 +69,67 @@ class GlobalMod {
         style.setAttribute('type', 'text/css');
         style.setAttribute('style', 'display:none;');
 
-        if (rule.style) {
-            style.textContent += rule.style;
+        style.textContent += rule.style;
+        if (rule.darkStyle !== undefined || rule.lightStyle !== undefined) {
+            style.textContent += GlobalMod.DarkMode ? 
+                rule.darkStyle : rule.lightStyle;
         }
 
-        if (rule['style-dark'] || rule['style-light']) {
-            style.textContent += GlobalMod.DarkMode ? 
-                    rule['style-dark'] : rule['style-light'];
-        }
-        
         return style;
     }
 
-    async applyStyles() {
-        const current = window.location.pathname.toLowerCase();
-        const editMode = window.location.search.includes('?edit=1');
-
-        for await (const [name, rule] of GlobalMod.instance.config) {
-            let style = GlobalMod.instance.styles.find(e => e.classList?.contains(name));
-
-            if (!current.includes(rule.path.toLowerCase())) {
-                if (style !== undefined) {
-                    style.remove();
-                }
-
-                continue;
+    async applyStyle(name, rule, current, editMode, style = undefined) {
+        if (!current.includes(rule.path.toLowerCase())) {
+            if (style !== undefined) {
+                style.remove();
             }
 
-            if (editMode && rule.disableOnEdit) {
-                continue;
-            }
+            return;
+        }
+
+        if (editMode && rule.disabledOnEdit) {
+            return;
+        }
+
+        if (style === undefined) {
+            style = await GlobalMod.instance.createStyleElement(rule);
+            style?.classList?.add(name);
+        }
+
+        try {
+            const tree = await GlobalMod.instance.selectTree(`home-assistant$${rule.selector}`, 1, 9);
             
-            if (style === undefined) {
-                style = await GlobalMod.instance.createStyleElement(rule);
-                style?.classList?.add(name);
+            if (!tree.contains(style)) {
+                tree.appendChild(style);
+                GlobalMod.instance.styles.push(style);
             }
-
-            try {
-                const tree = await GlobalMod.instance.selectTree(`home-assistant$${rule.selector}`, 1, 9);
-                
-                if (!tree.contains(style)) {
-                    tree.appendChild(style);
-                    GlobalMod.instance.styles.push(style);
-                }
-            } catch {
-                console.error(`Could not create rule ${name} after multiple tries...`);
-            }
+        } catch {
+            console.error(`Could not create rule ${name} after multiple tries...`);
         }
     }
 
-    loadConfig() {
+    async applyStyles() {
+        const current = GlobalMod.Current;
+        const editMode = GlobalMod.EditMode;
+
+        for await (const [name, rule] of GlobalMod.instance.config) {
+            let style = GlobalMod.instance.styles.find(e => e.classList?.contains(name));
+            GlobalMod.instance.applyStyle(name, rule, current, editMode, style);
+        }
+    }
+
+    async loadConfig() {
         let currentTheme = `${GlobalMod.instance.hass.themes.theme}-${GlobalMod.Name}`;
 
-        // Fall back to theme name without global-mod suffix. This should be 
+        // Fall back to theme name without global-mod suffix. This should be temporary.
         if (!(currentTheme in GlobalMod.instance.hass.themes.themes)) {
             currentTheme = `${GlobalMod.instance.hass.themes.theme}`;
         } else {
             console.warn(`Theme still uses the deprecated ${GlobalMod.Name}-suffix.`);
         }
 
-        GlobalMod.instance.config = new Map(
-            [...GlobalMod.instance.loadRules(currentTheme)]
-            .filter(([, r]) => r !== undefined)
-            .filter(([, r]) => r.path !== undefined)
-            .filter(([, r]) => r.selector !== undefined)
-        );                                            
+        const theme = GlobalMod.instance.hass.themes.themes[currentTheme];
+        GlobalMod.instance.config = await GlobalMod.instance.loadRules(theme);
 
         if (!GlobalMod.instance.config || GlobalMod.instance.config.size == 0) {
             console.info(`%c Global mod %c loaded without any config... \n  👉 Add a 'mods' section to your theme %c ${currentTheme} %c to enable modding.`,
@@ -141,24 +139,24 @@ class GlobalMod {
         }
     }
 
-    loadRules(currentTheme) {
+    async loadRules(theme) {
+        const current = GlobalMod.Current;
+        const editMode = GlobalMod.EditMode;
         const rules = new Map();
 
-        for (var k in GlobalMod.instance.hass.themes.themes[currentTheme]) {
-            const ruleKey = k.substring(0, k.indexOf('-'));
-            const ruleName = k.substring(k.indexOf('-') + 1);
+        for await (var k of Object.keys(theme).filter(e => e.includes('-selector'))) {
+            const ruleKey = k.substring(0, k.lastIndexOf("-"));
+            const rule = {
+                selector: theme[k],
+                path: theme[ruleKey + "-path"] || "/",
+                style: theme[ruleKey + "-style"] || "",
+                disabledOnEdit: theme[ruleKey + "-disable-on-edit"] || false,
+                darkStyle: theme[ruleKey + "-style-dark"],
+                lightStyle: theme[ruleKey + "-style-light"],
+            };
 
-            if (ruleKey.length === 0 || ruleName.length === 0) {
-                continue;
-            }
-
-            let rule = {};
-            if (rules.has(ruleKey)) {
-                rule = rules.get(ruleKey);
-            }
-
-            rule[ruleName] = GlobalMod.instance.hass.themes.themes[currentTheme][k];
             rules.set(ruleKey, rule);
+            GlobalMod.instance.applyStyle(ruleKey, rule, current, editMode);
         }
 
         return rules;
