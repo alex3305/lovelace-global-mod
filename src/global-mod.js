2305943984b0c72cdb9226348ee255d0d3bb6c2d
Alex van den Hoogen <alex3305@gmail.com>
2024-12-02 12:44:56 +0100

    Added docs

--- src/global-mod.js
+++ src/global-mod.js
@@ -1,47 +1,94 @@
 import { name, version } from '../package.json';
 
-class GlobalMod {
+"use strict";
 
-    #hass;
+/**
+ * GlobalMod for Home Assistant.
+ * 
+ * @author Alex van den Hoogen
+ */
+class GlobalMod {
 
     #config = [];
 
     #styles = [];
     
+    /**
+     * Creates a new GlobalMod instance. Also loads configuration,
+     * applies initial styling and adds event listeners.
+     */
     constructor() {
-        this.refreshHomeAssistant();
-
         Promise.all([
             this.loadConfig(),
             this.addEventListeners()
         ]);
     }
 
+    /**
+     * Gets the current path of the URL for the location in lowercase.
+     * 
+     * @returns {string} Current path of the URL.
+     */
     static get Current() { return window.location.pathname.toLowerCase(); }
 
+    /**
+     * Gets whether edit mode is enabled for the location.
+     * 
+     * @returns {boolean} True when edit mode is enabled.
+     */
     static get EditMode() { return window.location.search.includes('?edit=1'); }
     
-    static get Name() { return name; }
-    
-    static get Version() { return version; }
-    
-    get darkMode() { return this.#hass.themes.darkMode; }
-
-    async addEventListener(element, event, fn) {
-        element?.addEventListener(event, fn, false);
+    /**
+     * Reference to the Home Assistant HASS object that contains 
+     * the current configuration.
+     * 
+     * @returns {Object} Home Assistant frontend configuration.
+     */
+    static get Hass() {
+        return document.querySelector('home-assistant').hass;
     }
 
+    /**
+     * Gets the name of this component.
+     * 
+     * @returns {string} Name of this component.
+     */
+    static get Name() { return name; }
+    
+    /**
+     * Gets the current version of this component.
+     * 
+     * @returns {string} Current version of this component.
+     */
+    static get Version() { return version; }
+    
+    /**
+     * Gets whether the user has dark mode enabled in Home Assistant.
+     * 
+     * @returns {boolean} True when the user has dark mode enabled.
+     */
+    get darkMode() { return GlobalMod.Hass.themes.darkMode; }
+
+    /**
+     * Adds an event listener to a given target without capturing.
+     * 
+     * @param {EventTarget} target Target to add the event to.
+     * @param {string}      event  Event name.
+     * @param {(any)=>void} fn     Callback function.
+     */
+    async addEventListener(target, event, fn) {
+        target?.addEventListener(event, fn, false);
+    }
+
+    /**
+     * Adds all the required event listeners for Home Assistant.
+     */
     async addEventListeners() {
         const applyStylesEvent = () => {
             if (!document.hidden) {
                 this.applyStyles();
             }
         };
-
-        const refreshAndApplyStylesEvent = () => {
-            this.refreshHomeAssistant();
-            this.applyStyles();
-        }
 
         Promise.all([
             // Listen to location-changed for navigation.
@@ -64,51 +111,62 @@ class GlobalMod {
             // Reference: https://github.com/thomasloven/lovelace-card-mod/blob/f59abc785eabb689ac42a9076197254421f96c60/src/theme-watcher.ts
             this.addEventListener(document.querySelector('hc-main'), 
                                   "settheme",
-                                  refreshAndApplyStylesEvent),
+                                  applyStylesEvent),
             this.addEventListener(document.querySelector('home-assistant'), 
                                   "settheme",
-                                  refreshAndApplyStylesEvent)
+                                  applyStylesEvent)
         ]);
     }
 
-
-    async applyStyle(rule, current, editMode, style = undefined) {
-        if (!current.includes(rule.path.toLowerCase()) || 
-                (editMode && rule.disabledOnEdit)) {
+    /**
+     * Inserts or updates a specific style rule within the DOM and also
+     * manages  the backing array with styles.
+     * 
+     * @param {GlobalModRule} rule  GlobalMod Rule to apply.
+     * @param {Element}       style Existing style element to modify.
+     */
+    async applyStyle(rule, style = undefined) {
+        if (!GlobalMod.Current.includes(rule.path.toLowerCase()) || 
+                (GlobalMod.EditMode && rule.disabledOnEdit)) {
             style?.remove();
             return;
         }
 
         if (style === undefined) {
             style = await this.createStyleElement(rule);
-            style?.classList?.add(rule.name);
         }
 
         try {
             const tree = await this.selectTree(`home-assistant$${rule.selector}`, 1, 9);
             
             if (!tree.contains(style)) {
-                Promise.all([
-                    tree.append(style),
-                    this.#styles.push(style)
-                ]);
+                tree.append(style);
+                this.#styles.push(style);
             }
         } catch {
             console.error(`Could not create rule ${rule.name} after multiple tries...`);
         }
     }
 
+    /**
+     * Applies all the styles found in the loaded configuration.
+     */
     async applyStyles() {
         this.#config.forEach(rule => {
             this.applyStyle(
                 rule,
-                GlobalMod.Current,
-                GlobalMod.EditMode,
                 this.#styles.find(e => e.classList?.contains(rule.name))
             );
         });
     }
 
+    /**
+     * Creates a Global Mod Rule based on the current theme and selector.
+     * 
+     * @param {string} theme    The current selected Home Assistant theme.
+     * @param {string} selector The selector that is used to create the rule.
+     * @returns {GlobalModRule} Filled rule.
+     */
     async createRule(theme, selector) {
         const ruleName = selector.substring(0, selector.lastIndexOf("-"));
         
@@ -123,10 +181,17 @@ class GlobalMod {
         };
     }
 
+    /**
+     * Creates a HTML <style> element from a given rule.
+     * 
+     * @param {GlobalModRule} rule to create the style element from.
+     * @returns {Element} A style element.
+     */
     async createStyleElement(rule) {
         const style = document.createElement('style');
 
         style.classList?.add(GlobalMod.Name);
+        style.classList?.add(rule.name);
         style.setAttribute('style', 'display:none;');
 
         (async () => {
@@ -141,24 +206,26 @@ class GlobalMod {
         return style;
     }
 
+    /**
+     * Loads the current configuration and creates the mods from the given
+     * rules that are present within the user selected theme.
+     */
     async loadConfig() {
-        let currentTheme = `${this.#hass.themes.theme}-${GlobalMod.Name}`;
+        let currentTheme = `${GlobalMod.Hass.themes.theme}-${GlobalMod.Name}`;
 
         // Fall back to theme name without global-mod suffix. This should be temporary.
-        if (!(currentTheme in this.#hass.themes.themes)) {
-            currentTheme = `${this.#hass.themes.theme}`;
+        if (!(currentTheme in GlobalMod.Hass.themes.themes)) {
+            currentTheme = `${GlobalMod.Hass.themes.theme}`;
         } else {
             console.warn(`Theme still uses the deprecated ${GlobalMod.Name}-suffix.`);
         }
 
-        const theme = this.#hass.themes.themes[currentTheme];
+        const theme = GlobalMod.Hass.themes.themes[currentTheme];
         this.#config = await Promise.all(Object.keys(theme)
                 .filter(elem => elem.includes('-selector'))
                 .map(elem => this.createRule(theme, elem))
                 .map(elem => {
-                    (async () => {
-                        this.applyStyle(await elem, GlobalMod.Current, GlobalMod.EditMode)
-                    })();
+                    (async () => this.applyStyle(await elem))();
                     return elem;
                 }));
 
@@ -170,10 +237,14 @@ class GlobalMod {
         }
     }
 
-    refreshHomeAssistant() {
-        this.#hass = document.querySelector('home-assistant').hass;
-    }
-
+    /**
+     * Selects the tree to append the style element to.
+     * 
+     * @param {string} selector      Semi-DOM selector
+     * @param {number} iterations    Current iteration
+     * @param {number} maxIterations Maximum iterations to search
+     * @returns {Node} Found Node.
+     */
     async selectTree(selector, iterations, maxIterations) {
         let components = selector.split('$');
         let tree;
